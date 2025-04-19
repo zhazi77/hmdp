5de0f482292a0a924f5f59dafcb1f3bfc0a2e545
zhazi <zhazineedamail@163.com>
2025-04-19 22:18:33 +0800

    [done]: 完结撒花。

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -171,7 +171,21 @@ public class HmDianPingApplicationTests {
             }
             stringRedisTemplate.opsForGeo().add(key, locations);
         }
+    }
 
+    @Test
+    void testHyperLogLog() {
+        String[] values = new String[1000];
+        for (int i = 0; i < 1000000; i++) {
+            int j = i % 1000;
+            values[j] = "user_" + i;
+            if (j == 999) {
+                stringRedisTemplate.opsForHyperLogLog().add("hl2", values);
+            }
+        }
 
+        // 统计数量
+        Long count = stringRedisTemplate.opsForHyperLogLog().size("hl2");
+        System.out.println("count = " + count);
     }
 }
